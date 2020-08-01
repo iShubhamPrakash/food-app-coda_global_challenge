7ee0206f95f591cc27b8757c580115b68b835e45
Shubham Prakash <shubham.prakash2308@gmail.com>
2020-08-01 12:40:34 +0530

    deploy to firebase

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import NotFound from './NotFound';
 import Home from './Home/Home';
 import { FETCH_RECIPE_API } from "../constants/api";
 import DetailsPage from "./DetailPage/DetailsPage";
-
+import fallbackData from "../constants/fallbackApiData";
 
 function App() {
 
@@ -12,13 +12,19 @@ function App() {
 	const [loading, setLoading] = useState(true);
   
 
-
 useEffect(()=>{
   console.log("app")
   fetch(FETCH_RECIPE_API)
   .then(res=>res.json())
-  .then(data=>setRecipies(data))
+  .then(data=>{
+    if(!data) setRecipies(fallbackData)
+    setRecipies(data)
+  })
   .then(_=>setLoading(false))
+  .catch(e=>{
+    setRecipies(fallbackData)
+    setLoading(false)
+  })
 },[])
 
 
